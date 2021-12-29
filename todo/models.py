1011ffae9b2f8bacd538cdc1146bf81eb00f2e50
christelle01 <christellecarmela@gmail.com>
2021-12-29 11:52:13 +0000

    first commit

---
+++ todo/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+
+    title = models.CharField(max_length=255)
+    due_data = models.DateField()
+    completed = models.BooleanField()
+    favourite = models.BooleanField()
+
+    list = models.ForeignKey('TodoList', null=False, on_delete=models.CASCADE)
+
+class TodoList(models.Model):
+
+    name = models.CharField(max_length=255)
